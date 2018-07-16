d3d037109012ad9219011458b620285e034b7f8d
nkhuong11 <khuong.nguyen0322@gmail.com>
2018-07-16 15:42:15 +0700

    -a

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,21 @@
 <template>
   <div id="app">
-    Our project
+    <div id="nav">
+      Header Here
+    </div>
+    <div>
+      <router-view/>
+    </div>
+    <div>
+      Footer Here
+    </div>
+    
   </div>
 </template>
 
 <script>
-
-
-export default {
-  name: 'app',
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  export default {
+    name: 'app',
+    components: {}
+  }
+</script>>
--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import CartPage from '../views/CartPage.vue';
+
 Vue.use(Router)
 
 const router =  new Router({
   routes: [
-
+    {
+      path: '/cart',
+      name: 'cartPage',
+      component: CartPage
+    }
   ]
 })
 
