91d58aefd8b01d8bc27cfddf3e3b2d5d591a734d
Thanh BInh <nttbinh.spkt@gmail.com>
2018-07-20 21:32:00 +0700

    Update Nav bar and Footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,7 +13,7 @@
           <li v-show="isLogin" class="nav-item"><a href="#">LIVE UP</a></li>
           <li class="nav-item"><a href="#">Help</a></li>
           <li class="nav-item">
-            <a href="#"><font-awesome-icon icon="shopping-cart" style=""/>
+            <a href="#"><font-awesome-icon icon="shopping-cart"/>
             Cart
             </a> 
           </li>
@@ -23,12 +23,77 @@
         </ul>
       </nav>
     </header>
-    <div>
+    <div class="content">
       <router-view/>
     </div>
-    <div>
-      Footer Here
-    </div>
+    <footer>
+      <div class="footer-container-info">
+        <div class="footer-about">
+          <h4>About us</h4>
+          <ul>
+            <li><a href="#">Team</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Promotion</a></li>
+            <li><a href="#">Lazada SG</a></li>
+          </ul>
+        </div>
+        <div class="footer-contact">
+          <h4>Contact us</h4>
+          <ul>
+            <li><a href="#">Customer Support</a></li>
+            <li><a href="#">+84 xxxxxx</a></li>
+          </ul>
+        </div>
+        <div class="footer-help">
+          <h4>HElp</h4>
+          <ul>
+            <li><a href="#">Suggest a Product</a></li>
+            <li><a href="#">Price Guarantee</a></li>
+            <li><a href="#">Freshness Promise</a></li>
+            <li><a href="#">Fine Print for Coupons</a></li>
+            <li><a href="#">Security & Privacy Policy</a></li>
+            <li><a href="#">Terms of Use</a></li>
+          </ul>
+        </div>
+        <div class="footer-bussiness">
+          <h4>bussiness</h4>
+          <ul>
+            <li><a href="#">Sell on RedMart</a></li>
+            <li><a href="#">Marketing Enquiries</a></li>
+            <li><a href="#"> Press & Media</a></li>
+            <li><a href="#">Partner Portal</a></li>
+          </ul>
+        </div>
+        <div class="footer-app">
+          <h4>get the app</h4>
+          <ul>
+            <li><a href="#">Android</a></li>
+            <li><a href="#">IOS</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-container-payment">
+        <div class="delivery">
+          <img src="./assets/img/Delivery-512.png">
+          <p>At RedMart we have our very own fleet of delivery vans. Your order will be packed with care at our warehouse and delivered right to your door by our friendly RedMart team.</p>
+        </div>
+        <div class="payment">
+          <img src="./assets/img/Visa_Logo.png">
+          <img src="./assets/img/Mastercard-logo.png">
+          <img src="./assets/img/Paypal_logo.png">
+          <img src="./assets/img/AMR_EX.png">
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="social">
+          <a href="#"><img src="./assets/img/fb.png"></a>
+          <a href="#"><img src="./assets/img/twitter.png"></a>
+        </div>
+        <div class="copyright">
+          <p>Copyright © REDMART LIMITED. All Rights Reserved</p>
+        </div>
+      </div>
+    </footer>
     
   </div>
 </template>
@@ -52,6 +117,9 @@
   padding: 0;
   box-sizing: border-box;
 }
+body {
+  background-color: #d8dedf;
+}
 nav {
   position: fixed;
   background-color: #eb2c4c;
@@ -59,8 +127,10 @@ nav {
   height: 45px;
   font-size: 15px;
   line-height: 45px;
+  top: 0;
+  left: 0;
 }
-ul {
+nav ul {
   width: 75%;
   margin: 0 auto;
   list-style: none;
@@ -70,7 +140,7 @@ ul {
   text-align: center;
   align-items: center;
 }
-a {
+nav a {
   text-decoration: none;
   color: white;
 }
@@ -111,20 +181,106 @@ a {
   color: #eb2c4c;
 }
 @media only screen and (max-width: 1520px) {
-  ul {
+  nav ul,.footer-container-info,.footer-container-payment,.footer-container-copyright {
     width: 100%;
   }
+  
   .search-feild {
   flex-shrink: 1;
   }
 }
 @media only screen and (max-width: 1200px) {
-  ul {
+  nav ul,.footer-container-info {
     flex-wrap: wrap;
+  }
+
+  .footer-container-payment,.delivery {
+    flex-direction: column;
+  }
+  .footer-container-info ul {
+    flex-basis: 100%;
   }
   .search-feild {
     flex-shrink: 1;
     flex-basis: 100%;
   }
+}
+.content {
+  width: 75%;
+  min-height: 800px;
+  background-color: white;
+  margin: 45px auto 30px auto;
+}
+.footer-container-info {
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  padding: 40px 0;
+  flex-grow: 1;
+  justify-content: space-around;
+  color: #333;
+  border-top: 1px solid #adadad;
+  border-bottom: 1px solid #dadada;
+}
+.footer-container-info h4{
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 30px;
+}
+.footer-container-info ul{
+  font-size: 14px;
+  line-height: 30px;
+  font-family: 'Source Sans Pro',sans-serif;
+  list-style: none;
+}
+.footer-container-info ul li a{
+  text-decoration: none;
+  color: #333;
+}
+.footer-container-payment {
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+.delivery, .payment {
+  flex-basis: 50%;
+  flex-grow: 1;
+  flex-shrink: 1;
+  display: flex;
+  padding: 0 20px;
+}
+.footer-copyright {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+}
+.payment {
+    justify-content: space-around;
+}
+.delivery img {
+  max-width: 85px;
+  max-height: 85px;
+  border-radius: 50%;
+  background-color: white;
+}
+.delivery p {
+  text-justify: newspaper;
+  color: #333;
+  margin-left: 20px;
+}
+.payment img {
+  max-width: 80px;
+  max-height: 30px;
+}
+.social {
+  margin-bottom: 20px;
+}
+.social img {
+  max-width: 40px;
+  max-height: 40px;
+  border-radius: 50%;
 }
 </style>
