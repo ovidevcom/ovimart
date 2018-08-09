1573a9134dd5ec552bc6bbdea51d653d9f7c22a9
levanthanh-ptit <levanthanh.ptit@gmail.com>
2018-08-09 13:04:41 +0700

    update AccountInfo view

--- storefront/components/Account/AccountInfo/AccountInfo.js
+++ storefront/components/Account/AccountInfo/AccountInfo.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import "./AccountInfo.scss"
 import "../Account.scss"
+import { log } from 'util';
+
 
 export default class AccountInfo extends Component {
   state = {
@@ -8,7 +10,13 @@ export default class AccountInfo extends Component {
     lastName: "",
     email: "",
     contactNumber: "",
-    onEdit: null
+    onEdit: null,
+    errors: {
+      firstName: false,
+      lastName: false,
+      email: false,
+      contactNumber: false,
+    }
   }
 
   componentDidMount() {
@@ -51,12 +59,39 @@ export default class AccountInfo extends Component {
     })
   }
 
-  updateAccountInfo = () => {
-    this.validatePost()
+  nameCheck = function(str){
+    return !(/[A-Za-z]/.test(str));
+  }
+  emailCheck = (str) => {
+    return !(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(str));
+  }
+  contactNumberCheck = (str) => {
+    return !(/(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/.test(str));
   }
   validatePost = () => {
-  
+    var err = {
+      firstName: this.nameCheck(this.state.firstName),
+      lastName: this.nameCheck(this.state.lastName),
+      email: this.emailCheck(this.state.email),
+      contactNumber: this.contactNumberCheck(this.state.contactNumber)
+    }
+    console.log(err)
+    this.setState({
+      errors : err
+    })
+    if (err.firstName == true ||
+      err.lastName == true ||
+      err.email == true ||
+      err.contactNumber == true
+    ) {
+      return false
+    }
   }
+  updateAccountInfo = () => {
+    var x = this.validatePost()
+  }
+
+
   render() {
     if (this.state.onEdit == false)
       return (
@@ -90,7 +125,7 @@ export default class AccountInfo extends Component {
               </div>
             </div>
             <div className="saveButton">
-              <div className="Button Button_primary" onClick={this.switchToEdit.bind(this,true)}>Edit</div>
+              <div className="Button Button_primary" onClick={this.switchToEdit.bind(this, true)}>Edit</div>
             </div>
           </div>
         </section>
@@ -101,33 +136,57 @@ export default class AccountInfo extends Component {
           <div className="titles">
             <h4>Account Information</h4>
           </div>
-          <form id="changeInformation" className="rightPanel form" action="">
+          <form onSubmit={this.updateAccountInfo} id="changeInformation" className="rightPanel form" action="">
             <div>
               <div className="Full_container">
                 <div className="Half_container">
                   <label htmlFor="first_name">First Name</label>
                   <input type="text" name="firstName" id="first_name" onChange={this.editFirstName} value={this.state.firstName} maxLength="25" />
-                  <div className="validationErrorNote"></div>
+                  <div className="validationErrorNote">
+                    {this.state.errors.firstName &&
+                      <span>
+                        First Name is not valid
+                    </span>
+                    }
+                  </div>
                 </div>
                 <div className="Half_container">
                   <label htmlFor="last_name">Last Name</label>
-                  <input type="text" name="lastName" id="last_name" onChange={this.editLastName} value={this.state.lastName} maxLength="25"/>
-                  <div className="validationErrorNote"></div>
+                  <input type="text" name="lastName" id="last_name" onChange={this.editLastName} value={this.state.lastName} maxLength="25" />
+                  <div className="validationErrorNote">
+                    {this.state.errors.lastName &&
+                      <span>
+                        Last Name is not valid
+                    </span>
+                    }
+                  </div>
                 </div>
               </div>
               <div>
                 <label htmlFor="email">Email address</label>
                 <input type="email" name="email" id="email" onChange={this.editEmail} value={this.state.email} />
-                <div className="validationErrorNote"></div>
+                <div className="validationErrorNote">
+                  {this.state.errors.email &&
+                    <span>
+                      Email address is not valid
+                  </span>
+                  }
+                </div>
               </div>
               <div>
                 <label htmlFor="contact_number">Contact Number</label>
                 <input type="text" name="contactNumber" id="contact_number" onChange={this.editcontactNumber} value={this.state.contactNumber} />
-                <div className="validationErrorNote"></div>
+                <div className="validationErrorNote">
+                  {this.state.errors.contactNumber &&
+                    <span>
+                      Contact Number is not valid
+                  </span>
+                  }
+                </div>
               </div>
             </div>
             <div className="saveButton">
-              <div className="Button Button_primary" onClick={this.switchToEdit.bind(this,false)}>Save</div>
+              <div onClick={this.updateAccountInfo} className="Button Button_primary">Save</div>
             </div>
           </form>
         </section>
--- storefront/server.js
+++ storefront/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const next = require('next')
- const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
  app.prepare()
 .then(() => {
+  console.log("test mess")
   const server = express()
    server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
@@ -14,9 +15,9 @@ const handle = app.getRequestHandler()
    server.get('*', (req, res) => {
     return handle(req, res)
   })
-   server.listen(3001, (err) => {
+   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3001')
+    console.log('> Ready on http://localhost:3000')
   })
 })
 .catch((ex) => {
